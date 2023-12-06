9564203aab0fe0b59fd51403161e999f74af5280
Shintaro Inoue <s-inoue@jsk.imi.i.u-tokyo.ac.jp>
2023-12-06 12:11:56 +0900

    fix[example]: 引数nameが消えたことに対応、いらない関数を削除

--- tellopy/examples/video_effect_2.py
+++ tellopy/examples/video_effect_2.py
@@ -19,7 +19,7 @@ class TelloController:
     def __init__(self, port=9000, tello_ip='192.168.10.1', interface=None, video_port=6038, video=0, name='tello'):
         self.name = name
         self.video = video
-        self.drone = tellopy.Tello(port=port, tello_ip=tello_ip, interface=interface, video_port=video_port, name=name)
+        self.drone = tellopy.Tello(port=port, tello_ip=tello_ip, interface=interface, video_port=video_port)
         self.drone.connect()
         self.drone.wait_for_connection(10.0)
         retry = 3
@@ -86,83 +86,6 @@ class TelloController:
             self.log_data = data
         elif event is drone.EVENT_FLIGHT_DATA:
             self.flight_data = data
-
-
-def main():
-    global log_data
-    drone_blue = tellopy.Tello(port=20001, tello_ip='192.168.10.1', interface='wlp0s20f3')
-    drone_black = tellopy.Tello(port=20002, tello_ip='192.168.10.1', interface='wlx90de803fd246')
-
-    try:
-        print("===================1================")
-        drone_blue.connect()
-        drone_black.connect()
-        drone_blue.wait_for_connection(10.0)
-        drone_black.wait_for_connection(10.0)
-        print("===================2================")
-
-        retry = 3
-        container_blue = None
-        while container_blue is None and 0 < retry:
-            retry -= 1
-            try:
-                container_blue = av.open(drone_blue.get_video_stream())
-            except av.AVError as ave:
-                print(ave)
-                print('retry...')
-
-        retry = 3
-        container_black = None
-        while container_black is None and 0 < retry:
-            retry -= 1
-            try:
-                container_black = av.open(drone_black.get_video_stream())
-            except av.AVError as ave:
-                print(ave)
-                print('retry...')
-
-        # skip first 300 frames
-        frame_skip = 300
-        print("===================3================")
-        while True:
-            print("===================4================")
-            for frame_blue, frame_black in zip(container_blue.decode(video=0), container_black.decode(video=0)):
-                if 0 < frame_skip:
-                    frame_skip = frame_skip - 1
-                    continue
-                start_time = time.time()
-                image_blue = cv2.cvtColor(numpy.array(frame_blue.to_image()), cv2.COLOR_RGB2BGR)
-                cv2.imshow('Blue', image_blue)
-                cv2.imshow('Canny', cv2.Canny(image, 100, 200))
-                cv2.waitKey(1)
-                if frame_blue.time_base < 1.0 / 60:
-                    time_base = 1.0 / 60
-                else:
-                    time_base = frame_blue.time_base
-                frame_skip = int((time.time() - start_time) / time_base)
-
-                if 0 < frame_skip:
-                    frame_skip = frame_skip - 1
-                    continue
-                start_time = time.time()
-                image_black = cv2.cvtColor(numpy.array(frame_black.to_image()), cv2.COLOR_RGB2BGR)
-                cv2.imshow('Black', image_black)
-                # cv2.imshow('Canny', cv2.Canny(image, 100, 200))
-                cv2.waitKey(1)
-                if frame_black.time_base < 1.0 / 60:
-                    time_base = 1.0 / 60
-                else:
-                    time_base = frame_black.time_base
-                frame_skip = int((time.time() - start_time) / time_base)
-
-    except Exception as ex:
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        traceback.print_exception(exc_type, exc_value, exc_traceback)
-        print(ex)
-    finally:
-        drone_blue.quit()
-        drone_black.quit()
-        cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
